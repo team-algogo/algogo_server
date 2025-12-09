18928033db8cf47ceda0c1b2a8bac1c98b3cac6d
Teddysir <sdzx0719@naver.com>
2025-12-09 17:14:54 +0900

    fix: at/rt 제공 방식 변경

--- src/main/java/com/ssafy/algogo/auth/controller/AuthController.java
+++ src/main/java/com/ssafy/algogo/auth/controller/AuthController.java
@@ -2,7 +2,6 @@ package com.ssafy.algogo.auth.controller;
 
 import com.ssafy.algogo.auth.dto.request.LocalLoginRequestDto;
 import com.ssafy.algogo.auth.dto.response.AuthResultDto;
-import com.ssafy.algogo.auth.dto.response.LocalLoginResponseDto;
 import com.ssafy.algogo.auth.service.AuthService;
 import com.ssafy.algogo.auth.service.jwt.JwtTokenProvider;
 import com.ssafy.algogo.auth.service.security.CookieUtils;
@@ -30,7 +29,7 @@ public class AuthController {
     public SuccessResponse login(@RequestBody LocalLoginRequestDto requestDto, HttpServletRequest request, HttpServletResponse response) {
         AuthResultDto authResultDto = authService.login(requestDto, request, response);
 
-        CookieUtils.addTokenCookie(response, "accessToken", authResultDto.getTokenInfo().getAccessToken(), jwtTokenProvider.getAccessTokenValidTime());
+        response.setHeader("Authorization", authResultDto.getTokenInfo().getAccessToken());
         CookieUtils.addTokenCookie(response, "refreshToken", authResultDto.getTokenInfo().getRefreshToken(), jwtTokenProvider.getRefreshTokenValidTime());
 
         return SuccessResponse.success("로그인에 성공했습니다.", authResultDto.getLocalLoginResponseDto());
