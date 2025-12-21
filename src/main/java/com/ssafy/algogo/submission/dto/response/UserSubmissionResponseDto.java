bbe41fab9bc1dd8454014f191794a91c669f43d8
cramfull <tmdgud99@gmail.com>
2025-12-21 17:11:49 +0900

    feat: 프로그램문제로 제출 조회

--- src/main/java/com/ssafy/algogo/submission/dto/response/UserSubmissionResponseDto.java
+++ src/main/java/com/ssafy/algogo/submission/dto/response/UserSubmissionResponseDto.java
@@ -1,11 +1,13 @@
 package com.ssafy.algogo.submission.dto.response;
 
-import com.ssafy.algogo.problem.dto.response.ProgramProblemResponseDto;
+import com.ssafy.algogo.problem.dto.response.ProgramProblemDetailResponseDto;
 import com.ssafy.algogo.program.dto.response.ProgramResponseDto;
+import com.ssafy.algogo.user.dto.response.UserSimpleResponseDto;
 
 public record UserSubmissionResponseDto(
+    UserSimpleResponseDto user,
     SubmissionResponseDto submission,
-    ProgramProblemResponseDto programProblem,
+    ProgramProblemDetailResponseDto programProblem,
     ProgramResponseDto program
 ) {
 
